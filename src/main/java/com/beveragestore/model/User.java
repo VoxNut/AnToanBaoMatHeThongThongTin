9f0dbc309831fad56f5782e80f7cd69abcfaa59f
Võ Minh Nhựt <bnhut465@gmail.com>
2026-05-08 20:26:32 +0700

    merge: tích hợp nhánh Nhựt (hạ tầng, xác thực & chữ ký số)

--- src/main/java/com/beveragestore/model/User.java
+++ src/main/java/com/beveragestore/model/User.java
@@ -30,4 +30,48 @@ public class User implements Serializable {
     private String photoUrl;        // Google profile picture URL
     private Date createdAt;
     private boolean active;
+
+    // Cryptographic key management fields
+    private String activePublicKey;
+    private String activePublicKeyId;
+    private Date keyRevokedAt;
+    private List<PublicKeyRecord> keyHistory;
+
+    public static final String ROLE_ADMIN = "admin";
+    public static final String ROLE_CUSTOMER = "customer";
+    public static final String ROLE_SHIPPER = "shipper";
+    public static final String ROLE_SHOP_OWNER = "shop_owner";
+
+    public boolean isAdmin() {
+        return ROLE_ADMIN.equals(this.role);
+    }
+
+    public boolean isCustomer() {
+        return ROLE_CUSTOMER.equals(this.role);
+    }
+
+    public boolean isShipper() {
+        return ROLE_SHIPPER.equals(this.role);
+    }
+
+    public boolean isShopOwner() {
+        return ROLE_SHOP_OWNER.equals(this.role);
+    }
+
+    public boolean isGoogleUser() {
+        return "google".equals(this.authProvider);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class PublicKeyRecord implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private String keyId;
+        private String publicKeyPem;
+        private Date createdAt;
+        private Date revokedAt;
+    }
 }
+
